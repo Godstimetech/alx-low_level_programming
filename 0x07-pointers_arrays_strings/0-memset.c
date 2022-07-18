d8b9ebe81e3db906907fbc910e6e6f34af6adffb
Godstimetech <excellentmarketbiz@gmail.com>
2022-07-18 12:53:41 -0700

    o task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _memset -> this memory set function
+* @s: string
+* @b: x charcter
+* @n: an integer
+* Return:  x string
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+for (x = 0; x < n; a++)
+s[x] = b;
+return (s);
+}
