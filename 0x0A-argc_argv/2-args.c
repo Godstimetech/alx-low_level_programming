6cd50fbe7f5314191eff04ad3649d9e11b12f2fe
Godstimetech <excellentmarketbiz@gmail.com>
2022-07-22 12:55:42 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 /**
- *
- * * main -> this is a function to print its name
- *
- * * @argc: argc parameter
- *
- * * @argv: an array of a command listed
- *
- * * Return: 0 for success
- *
- * */
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 int main(int argc, char *argv[])
 {
 int i;
