00c7d2db530545a7780d5dee46939066356106be
Godstimetech <excellentmarketbiz@gmail.com>
2022-07-26 17:14:57 -0700

    task 0 resolve

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,14 @@
 */
 char *create_array(unsigned int size, char c)
 {
-unsigned int x;
+unsigned int i;
 char *s;
 if (size == 0)
 return (NULL);
 s = (char *)malloc(size * sizeof(char));
 if (s == NULL)
 return (NULL);
-for (x = 0; x < size; x++)
-s[x] = c;
+for (i = 0; i < size; i++)
+s[i] = c;
 return (s);
 }
