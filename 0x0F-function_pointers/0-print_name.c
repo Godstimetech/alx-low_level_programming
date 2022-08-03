af39008fdfd57ea3d79107bc7b8b2d47381b2c7e
Godstimetech <excellentmarketbiz@gmail.com>
2022-08-03 15:55:37 -0700

    funtion commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h> 
+/**
+* print_name - prints a name.
+* @name: name to be printed;
+* @f: function pointer
+* Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (!f)
+return;
+f(name);
+}
